a326be60f543a966f19d9703d2e07698f435dfa2
fahad sirkazi <fahadsirkazi109@gmail.com>
2025-09-11 21:20:40 +0530

    Siri wave design completed

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+# playing Assistant sound
+def playAssistantSound():
+    music_dir = "www\\assets\\audio\\www_assets_audio_start_sound.mp3"
+    playsound(music_dir)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 
 import eel
+from engine.features import*
 eel.init("www")
+playAssistantSound()
 
 os.system('start brave.exe --app="http://localhost:8000/index.html"')
 eel.start('index.html',mode=None ,host='localhost',block=True)
