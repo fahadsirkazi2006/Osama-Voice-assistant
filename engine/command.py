6718aff991743fc52e3137fc965934b47e5df5c2
fahad sirkazi <fahadsirkazi109@gmail.com>
2025-09-24 19:55:05 +0530

    speech recognizers

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,5 @@
 import pyttsx3
-
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -11,4 +11,23 @@ def speak(text):
     engine.runAndWait()
 
 
-speak("I am OSAMA")
+def takecommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold=1
+        r.adjust_for_ambient_noise(source)
+        audio=r.listen(source,10,6)
+        
+
+        
+
+    try:
+        print('recognizing..')
+        query=r.recognize_google(audio,language='en-in')
+        print(f'user said: {query}')
+    except Exception as e:
+        return ''
+    return query.lower()
+text= takecommand()
+speak(text)
