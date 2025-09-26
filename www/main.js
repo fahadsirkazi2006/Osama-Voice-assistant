f227881d24bfa76af8af8b546eabe7ce9ac3f535
fahad sirkazi <fahadsirkazi109@gmail.com>
2025-09-26 19:50:54 +0530

    connecting frontend and backend

--- www/main.js
+++ www/main.js
@@ -41,6 +41,6 @@ $(document).ready(function () {
         eel.playAssistantSound()
        $("#oval").attr("hidden", true);
        $("#SiriWave").attr("hidden", false);
-        
+        eel.takecommand()()
     });
 });
